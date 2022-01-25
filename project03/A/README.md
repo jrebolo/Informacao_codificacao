0b1158d811364ee20a2474928d8ab37731f049e9
joao <joaop.vieira16@gmail.com>
2022-01-25 16:41:32 +0000

    update files and and more txt

---
+++ project03/A/README.md
@@ -0,0 +1,5 @@
+## How to compile
+
+```c++
+g++ -std=c++17 -o2 main.cpp -o a.out
+```
---
+++ project03/A/main.cpp
@@ -0,0 +1,41 @@
+#include "fcm.h"
+#include <getopt.h>
+
+int main(int argc,char* argv[]){
+  
+  double entropy;
+  std::string filename;
+  int opt,h,f;
+  int k = 1;
+  double a = 0.1;
+  while ((opt = getopt(argc,argv,"hf:a:k:")) != EOF){
+    switch(opt){
+        case 'a': 
+                  a = atof(optarg); 
+                  if (a <= 0) a = 0.1;
+                  break;
+        case 'k': 
+                  k = atoi(optarg);
+                  if (k <= 0) k = 1;
+                  break;
+        case 'f': 
+                  filename = optarg; 
+                  std::cout << filename << "\n";
+                  break;
+        case 'h': fprintf(stderr, "usuage is \n -a : <value> \n -f : [filename] \n -k: <value> \n");
+                  break;
+    }
+  }
+
+  Fcm fcm;
+  if(fcm.open(filename)){
+    printf("Could not open file\n");
+    return -1;
+  }
+  
+  fcm.save_text();
+  fcm.count_depth(k,a);
+  fcm.entropy();
+  fcm.close(); 
+  return 0;
+}
